8cfec2d49b88ef7788d418a0bc7fb49d5b4e1122
sashimi-rtx <yan2019lima@gmail.com>
2025-08-21 16:14:36 -0300

    me odeio

---
+++ docs/config.php
@@ -0,0 +1,19 @@
+<?php
+
+ $dbHost = 'Localhost';
+ $dbUsername = 'root';
+ $dbPassword = '';
+ $dbName = 'formulari'
+
+ $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+ // if($conexao->connect_errno)
+ // {
+ //   echo 'Error';
+ // }
+ // else
+ // {
+ // echo 'Conexão bem sucedida';
+ // }
+
+ ?>
